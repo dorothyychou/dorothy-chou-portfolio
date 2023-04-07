ca0496a0e8a0612ff35f7c36c2ea098f894d89e6
dorothyychou <dchou@ucsd.edu>
2023-04-06 17:51:40 -0700

    portfolio

---
+++ changelog.md
@@ -0,0 +1,10 @@
+## Content Changes
+- added a section in my about me page for my interests
+- edited alt text for photos and videos to be more descriptive
+- cleaned up the code (removed commented sections from previous assignments)
+
+## JavaScript Changes
+- added a photo of the cat who often visits me outside my home named tiger when the "click for a surprise!" button on the homepage is clicked
+  - the purpose for adding this is to add a little  personal touch to my portfolio website
+- added functionality to the hamburger menu when the screen width is small (scaled for mobile)
+  - the purpose of the working hamburger menu is to allow navigation on mobile, which is important for the accessibility of my site
---
+++ hammenu.js
@@ -0,0 +1,28 @@
+const smallnav = document.getElementById("mini-nav");
+const smallmenu = document.querySelector(".small-menu");
+const menuitems = document.querySelectorAll(".small-menu-item");
+const hamburger= document.getElementById("hamburger");
+const closebtn= document.getElementById("close-btn");
+const menubtn = document.getElementById("ham-menu");
+
+function toggleMenu() {
+  if (smallmenu.classList.contains("show-menu")) {
+    smallmenu.classList.remove("show-menu");
+    smallnav.style.display = "none";
+    closebtn.style.display = "none";
+    menubtn.style.display = "block";
+  } else {
+    smallmenu.classList.add("show-menu");
+    smallnav.style.display = "inline";
+    closebtn.style.display = "block";
+    menubtn.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuitems.forEach( 
+  function(menuitem) { 
+    menuitem.addEventListener("click", toggleMenu);
+  }
+)
